e9a1829d0f17eb5e3444fff40c437c6e0447a22d
AcquaWh <acquaentertainment@gmail.com>
2019-05-13 10:29:36 -0700

    GameMaster Error

---
+++ Assets/Scripts/Core/TalkSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkSystem : MonoBehaviour
+{
+    IEnumerator<WaitForSeconds> checkConv;
+    [SerializeField]
+    GameObject talkSystemUI;
+    [SerializeField]
+    Text textMessage;
+    [SerializeField]
+    string message = "";
+
+    public IEnumerator<WaitForSeconds> CheckConv { get => checkConv; set => checkConv = value; }
+
+    private void Start()
+    {
+        checkConv = CheckConversation();
+        StartCoroutine(checkConv);
+        InitConversation();
+    }
+
+
+    public void InitConversation()
+    {
+        StartCoroutine(checkConv);
+    }
+
+    public IEnumerator<WaitForSeconds> CheckConversation()
+    {
+        talkSystemUI.SetActive(true);
+        textMessage.text = message;
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            if (!talkSystemUI.activeSelf)
+            {
+                talkSystemUI.SetActive(true);
+            }
+        }
+    }
+}
--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -7,6 +7,9 @@ public class Gamemanager : MonoBehaviour
     public static Gamemanager instance;
     [SerializeField]
     PartySystem partySystem;
+    [SerializeField]
+    TalkSystem talkSystem;
+
     void Awake()
     {
         if (!instance)
@@ -20,11 +23,29 @@ public class Gamemanager : MonoBehaviour
         }
 
         partySystem.StartParty();
+        talkSystem = GetComponent<TalkSystem>();
     }
+    private void Start()
+    {
+        talkSystem.InitConversation();
+        StartCoroutine(talkSystem.CheckConversation());
+        talkSystem.CheckConv = talkSystem.CheckConversation();
+        StartCoroutine(talkSystem.CheckConv);
 
+    }
+    public void InitConversation()
+    {
+        talkSystem.InitConversation();
+    }
     private void Update()
     {
         if (ControllerSystem.Swap) partySystem.SwapLeader();
+        if (Input.GetButtonDown("Submit"))
+        {
+            StopAllCoroutines();
+            talkSystem.InitConversation();
+
+        }
     }
 
     public PartySystem PartySystem
